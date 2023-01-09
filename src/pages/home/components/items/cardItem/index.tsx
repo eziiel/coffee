de232dea3b13e7b4b77946977580f6c867039597
eziiel <eziielp10@hotmail.com>
2023-01-09 08:36:43 -0300

    count coffees item

--- src/pages/home/components/items/cardItem/index.tsx
+++ src/pages/home/components/items/cardItem/index.tsx
@@ -36,13 +36,7 @@ export const CoffeeCardItem = ({
           <S.PriceCifre>RS</S.PriceCifre>
           <S.PriceNumber>{price}</S.PriceNumber>
         </S.CoffeePrice>
-        <ControledAmount
-          amount={5}
-          id={id}
-          title={title}
-          price={price}
-          img={img}
-        />
+        <ControledAmount id={id} title={title} price={price} img={img} />
         <S.CoffeeCart>
           <ShoppingCart size={24} color="#fff" weight="fill" />
         </S.CoffeeCart>
